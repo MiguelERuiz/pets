abc79c334c92daa0d711f58f4b334ff5baad0bb2
Miguel E. Ruiz <meruiz77@gmail.com>
2024-02-24 01:44:26 +0100

    WIP `pet` macro

--- lib/pets/pet.ex
+++ lib/pets/pet.ex
@@ -1,14 +1,22 @@
 defmodule Pets.Pet do
-  defmacro pet(pet_name) do
+  require Logger
+
+  defmacro pet(pet_name, do: block) do
+    {:__block__, _metadata, do_block} = block
+    [{:species, _, [species]}, {:hobbies, _, [hobbies]}] = do_block
     normalized_module = pet_name |> String.to_atom
     contents =
       quote do
         def greet do
-          IO.puts("Hello there, my name is #{unquote(pet_name)}")
+          """
+            Hello there, my name is #{unquote(pet_name)} and I am a #{unquote(species)}
+            I love ...
+          """
+          |> IO.puts
         end
 
         def hobbies do
-          ["spend good time"]
+          unquote(hobbies)
         end
       end
     __CALLER__.module |>
--- lib/template.ex
+++ lib/template.ex
@@ -1,5 +1,16 @@
 defmodule Template do
   import Pets.Pet
 
-  pet "Bucky"
+  pet "Bucky" do
+    species :dog
+    hobbies ["sniffing", "eating birds"]
+  end
+
+  # pet "Atila" do
+  #   hobbies ["napping"]
+  # end
+
+  # pet "Godzilla" do
+  #   hobbies ["destroying Tokio"]
+  # end
 end
